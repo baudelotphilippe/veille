fc4eb814bf4135ac5436d3274f7333cf18d8d9fb
baudelotphilippe <philippe@baudelot.eu>
2022-11-09 16:26:49 +0100

    add Lien

---
+++ back/src/Entity/Lien.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\LienRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=LienRepository::class)
+ */
+class Lien
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
