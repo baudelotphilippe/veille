ddb4c3e30898a77da8906307270aca97a5e958e0
baudelotphilippe <philippe@baudelot.eu>
2022-11-10 19:04:45 +0100

    display liens

--- back/assets/js/components/Home.js
+++ back/assets/js/components/Home.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// require('../css/app.scss');
+import axios from 'axios';
 
 class Home extends React.Component {
+
+  state = {
+    liens: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/liens`)
+      .then(res => {
+        const liens = res.data["hydra:member"];
+        console.log(liens)
+        this.setState({ liens });
+      })
+  }
+
   render() {
     return (
-      <h1>Hello world</h1>
-    );
+      <ul>
+        {
+          this.state.liens
+            .map(lien =>
+              <li key={lien.id}>{lien.url}</li>
+            )
+        }
+      </ul>
+    )
   }
 }
+
 export default Home;
