d223505d0e604c3403e0de40e82ee63f7f20d2ac
baudelotphilippe <philippe@baudelot.eu>
2022-11-10 18:28:50 +0100

    react Hello world

---
+++ back/assets/js/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// require('../css/app.scss');
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    );
+  }
+}
+export default Home;
