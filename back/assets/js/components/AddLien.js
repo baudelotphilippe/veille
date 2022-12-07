11b305d2b7d0792add429b1563c90f9f534b3dd4
baudelotphilippe <philippe@baudelot.eu>
2022-12-07 11:41:35 +0100

    refacto as hook

--- back/assets/js/components/AddLien.js
+++ back/assets/js/components/AddLien.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-export default class AddLien extends React.Component {
-  state = {
-    url: ''
+const AddLien=() =>{
+  const [url, setUrl]= useState('')
+
+
+  const handleChange = event => {
+    setUrl(event.target.value);
   }
 
-  handleChange = event => {
-    this.setState({ url: event.target.value });
-  }
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
 
-    const url = {
-      url: this.state.url
-    };
+    // const url = {
+    //   url: url
+    // };
 
-    axios.post(`http://127.0.0.1:8000/api/liens`, url )
+    axios.post(`http://127.0.0.1:8000/api/liens`, {url} )
       .then(res => {
         console.log(res);
         console.log(res.data);
-        this.props.loadAll()
+        // this.props.loadAll()
       })
   }
 
-  render() {
     return (
       <div className='p-4 mb-4'>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label>
             URL 
-            <input className='ms-2' type="text" name="url" onChange={this.handleChange} placeholder="http://..."/>
+            <input className='ms-2' type="text" name="url" onChange={handleChange} value={url} placeholder="http://..."/>
           </label>
           <button className='btn btn-sm btn-primary ms-2' type="submit">Ajouter</button>
         </form>
       </div>
     )
   }
-}
+
+
+  export default AddLien
