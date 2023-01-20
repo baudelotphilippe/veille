32525a63fcc62425c445d2642a2f5de99a69bd5e
baudelotphilippe <philippe@baudelot.eu>
2023-01-20 08:40:36 +0100

    reorg bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,3 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// // any CSS you import will output into a single css file (app.css in this case)
-// import './styles/app.css';
-
-// // start the Stimulus application
-// import './bootstrap';
-require("bootstrap/dist/css/bootstrap.css");
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -21,6 +7,9 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import { createRoot } from 'react-dom/client';
 import * as AxiosService from './services/AxiosService';
+// import 'jquery/dist/jquery.min.js';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 AxiosService.verifTokenExist();
 
