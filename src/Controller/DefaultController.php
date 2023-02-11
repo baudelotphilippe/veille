91335af27cdd603939ff8b7c77d19227b7b25c77
baudelotphilippe <philippe@baudelot.eu>
2023-02-11 08:44:40 +0100

    change Security

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -54,14 +54,14 @@ class DefaultController extends AbstractController
     //             'imageURL' => 'https://randomuser.me/api/portraits/men/89.jpg'
     //         ]
     //     ];
-    
+
     //     $response = new Response();
 
     //     $response->headers->set('Content-Type', 'application/json');
     //     $response->headers->set('Access-Control-Allow-Origin', '*');
 
     //     $response->setContent(json_encode($users));
-        
+
     //     return $response;
     // }
 }
